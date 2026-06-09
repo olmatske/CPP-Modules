70dbc9a97360e204afe3bd4254a4bcc300d23cd5
Olga Matskevich <olmatske@3-F-10.42heilbronn.de>
2026-06-09 14:43:02 +0200

    fixed minor issue in ex00

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -6,12 +6,12 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 12:57:03 by olmatske          #+#    #+#             */
-/*   Updated: 2026/06/08 14:38:27 by olmatske         ###   ########.fr       */
+/*   Updated: 2026/06/09 14:01:17 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
+#include <std::string>
 #include <cctype>
 
 using namespace std;
@@ -20,17 +20,17 @@ int	main(int argc, char **argv)
 {
 	(void)argc;
 	if (argc == 1)
-		cout << "* LOUD AND UBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; argv[i]; i++)
 		{
-			string	str = argv[i];
+			std::string	str = argv[i];
 			for (size_t k = 0; k < str.length(); k++)
 				str[k] = toupper(static_cast<unsigned char>(str[k]));
-			cout << str << " ";
+			std::cout << str << " ";
 		}
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
