0f3b991b139f8228e5b86d87b30c1a7487e688a2
Olga Matskevich <olmatske@3-F-11.42heilbronn.de>
2026-06-29 19:27:15 +0200

    wrote header, relocating

---
+++ 3/ex00/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*              ／＞　 フ                        ˚★ ₊ ⊹　  ࣪˖ ࣪ ࣪ ˖⋆˚★ ₊ ⊹　  ࣪˖ ࣪ ₊  ࣪ ˖*/
+/*             | 　_　_|                             ˖ ࣪　⊹ ࣪ ★⊹ ࣪ ┆ ˖ ࣪　⊹ ࣪ ★ ⋆.˚  ⊹ ࣪*/
+/*            ／` ミ＿xノ                                    ࣪ ˖⋆˚★ ₊ ⊹　  ࣪˖ ࣪ ₊  ࣪ ˖*/
+/*           /　　　　 |                                    ˖⋆˚★ ₊ ⊹　  ࣪˖ ࣪ ₊  ࣪ ˖　*/
+/*          /　 ヽ　　 ﾉ           へ   ╱|、 ♡  ╱|、        ☆⊹ ࣪ ┆ ˖ ࣪　⊹ ࣪ ★ .˚  ⊹ */
+/*          │　　|　|　|       ૮ - ՛ ) (˚ˎ 。7  (` -  7           ˚★ ₊ ⊹　  ࣪˖ ࣪ ₊ */
+/*      ／￣ 　　 |　|　|       /  ⁻៸|  |、 ˜(    |、⁻(           ★ ₊ ⊹　  ࣪˖ ࣪ ࣪ ˖⋆ */
+/*      ( ( ヽ＿_ヽ_)__)   乀 (ˍ, ل ل  じしˍ,)ノ じしˍ,)ノ         　⊹ ࣪ ★ ⋆.˚    */
+/*      ＼__二)                                               ࣪˖ ࣪ ₊  ★⊹ ࣪ ┆  ★ ⋆.*/
+/* ********************************************************** github/olmatske */
+
+#ifndef __CLAPTRAP_HPP__
+#define __CLAPTRAP_HPP__
+
+#include <iostream>
+
+class ClapTrap {
+	private:
+		std::string _name;
+		int			_hitpoints;
+		int			_energy;
+		int			_dmg;
+
+	public:
+		Fixed	
+
+		void	attack( const std::string &target );
+		void	takeDamage( unsigned int amount );
+		void	beRepaired( unsigned int amount );
+
+};
+
+
+#endif
