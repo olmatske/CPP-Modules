cc9e3b4b27498fea8bc0a9be1addf884a74d0658
Olga Matskevich <olmatske@1-E-20.42heilbronn.de>
2026-07-13 20:39:44 +0200

    working on CPP04 now

---
+++ 4/ex00/Animal.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*              ／＞　 フ                        ˚★ ₊ ⊹　  ࣪˖ ࣪ ࣪ ˖⋆˚★ ₊ ⊹　  ࣪˖ ࣪ ₊  ࣪ ˖*/
+/*             | 　_　_|                             ˖ ࣪　⊹ ࣪ ★⊹ ࣪ ┆ ˖ ࣪　⊹ ࣪ ★ ⋆.˚  ⊹ ࣪*/
+/*            ／` ミ＿xノ                                    ࣪ ˖⋆˚★ ₊ ⊹　  ࣪˖ ࣪ ₊  ࣪ ˖*/
+/*           /　　　　 |                                    ˖⋆˚★ ₊ ⊹　  ࣪˖ ࣪ ₊  ࣪ ˖　*/
+/*          /　 ヽ　　 ﾉ           へ   ╱|、 ♡  ╱|、        ☆⊹ ࣪ ┆ ˖ ࣪　⊹ ࣪ ★ .˚  ⊹ */
+/*          │　　|　|　|       ૮ - ՛ ) (˚ˎ 。7  (` -  7           ˚★ ₊ ⊹　  ࣪˖ ࣪ ₊ */
+/*      ／￣ 　　 |　|　|       /  ⁻៸|  |、 ˜(    |、⁻(           ★ ₊ ⊹　  ࣪˖ ࣪ ࣪ ˖⋆ */
+/*      ( ( ヽ＿_ヽ_)__)   乀 (ˍ, ل ل  じしˍ,)ノ じしˍ,)ノ         　⊹ ࣪ ★ ⋆.˚    */
+/*      ＼__二)                                               ࣪˖ ࣪ ₊  ★⊹ ࣪ ┆  ★ ⋆.*/
+/* ********************************************************** github/olmatske */
+
+#ifndef __ANIMAL_HPP__
+#define __ANIMAL_HPP__
+
+#include <iostream>
+#include <string>
+
+class Animal {
+	protected:
+		std::string	_type;
+	
+	public:
+		Animal( std::string type );
+		Animal( const Animal &other );
+		Animal& operator=( const Animal &other );
+		~Animal();
+
+		void	makeSound();
+;}
+
+
+#endif
